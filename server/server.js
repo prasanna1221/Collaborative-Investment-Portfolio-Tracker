4a57e3ef66a357c678c2d3cae944b01fd0e1e063
prasanna1221 <prasannaravi36@gmail.com>
2024-12-09 23:44:42 +0530

    updated server code

--- server/server.js
+++ server/server.js
@@ -6,3 +6,14 @@ const {server} = require("socket.io");
 const http = require("http");
 
 dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//Database connection
+mongoose
+    .connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>console.log("MongoDB connected"))
+    .catch((error)=> console.log(error));
+
