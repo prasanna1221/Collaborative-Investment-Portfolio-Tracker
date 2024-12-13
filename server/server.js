97401ec17007c4d5d4a5ddd3b2fa3bd36ea28707
prasanna1221 <prasannaravi36@gmail.com>
2024-12-13 16:59:56 +0530

    update

--- server/server.js
+++ server/server.js
@@ -17,3 +17,33 @@ mongoose
     .then(()=>console.log("MongoDB connected"))
     .catch((error)=> console.log(error));
 
+
+// Import Routes
+const userRoutes = require("./routes/userRoutes");
+const portfolioRoutes = require("./routes/portfolioRoutes");
+
+app.use("/api/users", userRoutes);
+app.use("/api/portfolio", portfolioRoutes);
+
+// Set up WebSocket server
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+
+  socket.on("update-portfolio", (data) => {
+    io.emit("portfolio-updated", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
